0ef821eb8e5b9139d3c6f7445667602dd58358fb
Julian <yenhachi8@gmail.com>
2020-08-26 17:03:54 +0800

    ad 1.3.py

---
+++ pythonPractice/1.3.py
@@ -0,0 +1,12 @@
+x=int(input("Input x:"))
+y=int(input("Input y:"))
+u=int(input("Input u:"))
+v=int(input("Input v:"))
+
+print("x= "+str(x)+" y="+str(y)+" u="+str(u)+" v="+str(v))
+t1=x+y
+t2=u+v
+
+z=t1*t2
+print("Asn: ",z)
+
