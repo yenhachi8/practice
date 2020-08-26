76cfe1140a238fa633ceec0564ecfefea3ca64ee
Julian <yenhachi8@gmail.com>
2020-08-26 23:28:17 +0800

    ad

---
+++ pythonPractice/2.2compare2.py
@@ -0,0 +1,8 @@
+x = int(input("Input x:"))
+y = int(input("Input y:"))
+print("x= "+str(x)+" y="+str(y))
+if x > y:
+    z = x-y
+else:
+    z = y-x
+print("z= "+str(z))
---
+++ pythonPractice/2.3.py
@@ -0,0 +1,10 @@
+x = int(input("Input x:"))
+print("x= "+str(x))
+
+if x >= 90:
+    grade = 'A'
+elif x >= 80:
+    grade = 'B'
+else:
+    grade = 'F'
+print(grade)
