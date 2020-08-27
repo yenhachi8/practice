b9c500143fd53a7776cd1593202ae454b2dd6c54
Julian <yenhachi8@gmail.com>
2020-08-27 20:08:09 +0800

    99

---
+++ 99.py
@@ -0,0 +1,8 @@
+x = 1
+y = 1
+for i in range(1, 10):
+    for j in range(1, 10):
+      #  print("%d*%d=%2d" % (i, j, i*j), sep='', end=' ')
+
+        print("%2d" % (i*j), sep='', end=' ')
+    print()
